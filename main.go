5e2a6fc304f58af8cc9841717e5ce7cc89373412
shelepuginivan <shelepuginivanm@gmail.com>
2026-07-11 00:44:26 +0300

    app: add basic build process

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/shelepuginivan/makewww/pkg/dist"
+	"github.com/shelepuginivan/makewww/pkg/source"
+)
 
 func main() {
-	fmt.Println(".")
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	src := source.FromProjectRoot(cwd)
+	dist := dist.FromRoot(filepath.Join(cwd, "dist"))
+
+	docs, err := src.GetDocuments()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, doc := range docs {
+		p, err := doc.CanonicalPath(src.ContentDir())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		f, err := dist.CreateOutputFile(p)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		if err := doc.Render(f); err != nil {
+			log.Fatal(err)
+		}
+	}
 }

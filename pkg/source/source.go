f576f1d7187c4b2b1087d5fe0d92f96163612af1
shelepuginivan <shelepuginivanm@gmail.com>
2026-07-11 00:15:28 +0300

    source: get documents recursively

--- pkg/source/source.go
+++ pkg/source/source.go
@@ -2,8 +2,12 @@ package source
 
 import (
 	"fmt"
+	"io/fs"
 	"path/filepath"
+	"strings"
 	"text/template"
+
+	"github.com/shelepuginivan/makewww/pkg/document"
 )
 
 const (
@@ -19,12 +23,46 @@ func FromProjectRoot(root string) *Source {
 	return &Source{root: root}
 }
 
+func (src *Source) GetDocuments() ([]document.Document, error) {
+	var documents []document.Document
+
+	err := filepath.Walk(filepath.Join(src.root, ContentDir), func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		var d document.Document
+
+		switch {
+		case strings.HasSuffix(path, ".html.tmpl"):
+			d = document.HTMLFromPath(path)
+		case strings.HasSuffix(path, ".md.tmpl"):
+			d = document.MarkdownFromPath(path)
+		default:
+			d = document.RawFromPath(path)
+		}
+
+		documents = append(documents, d)
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get documents: %w", err)
+	}
+
+	return documents, nil
+}
+
 func (src *Source) GetTemplate(path string) (*template.Template, error) {
 	if !filepath.IsAbs(path) {
 		return nil, fmt.Errorf("template path must be absolute")
 	}
 
-	tmpl, err := template.ParseFiles()
+	tmplPath := filepath.Join(src.root, path)
+	tmpl, err := template.ParseFiles(tmplPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get template: %w", err)
 	}

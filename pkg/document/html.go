dfb9acc94726e1f5dc8e46fafa76b9fb15b420cf
shelepuginivan <shelepuginivanm@gmail.com>
2026-07-10 23:39:24 +0300

    document: add base html struct

---
+++ pkg/document/html.go
@@ -0,0 +1,28 @@
+package document
+
+import (
+	"fmt"
+	"io"
+	"text/template"
+)
+
+type HTML struct {
+	path string
+}
+
+func HTMLFromPath(path string) *HTML {
+	return &HTML{path: path}
+}
+
+func (h *HTML) Render(w io.Writer) error {
+	tmpl, err := template.ParseFiles(h.path)
+	if err != nil {
+		return fmt.Errorf("failed to parse html document: %w", err)
+	}
+
+	if err := tmpl.Execute(w, struct{}{}); err != nil {
+		return fmt.Errorf("failed to render html document: %w", err)
+	}
+
+	return nil
+}

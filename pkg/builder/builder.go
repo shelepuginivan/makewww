0e779996e47cfa4e8166593bbe1afc55f757c81b
shelepuginivan <shelepuginivanm@gmail.com>
2026-07-11 04:57:27 +0300

    builder: use new source methods

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -1,6 +1,8 @@
 package builder
 
 import (
+	"fmt"
+
 	"github.com/shelepuginivan/makewww/pkg/config"
 	"github.com/shelepuginivan/makewww/pkg/dist"
 	"github.com/shelepuginivan/makewww/pkg/source"
@@ -26,26 +28,66 @@ func New(cfg *config.Config) *Builder {
 }
 
 func (b *Builder) Build() error {
-	docs, err := b.src.GetDocuments()
+	documents, err := b.src.Documents()
+	if err != nil {
+		return fmt.Errorf("failed to get documents: %w", err)
+	}
+
+	rawFiles, err := b.src.RawFiles()
+	if err != nil {
+		return fmt.Errorf("failed to get raw files: %w", err)
+	}
+
+	for _, doc := range documents {
+		if err := b.renderDocument(doc); err != nil {
+			return fmt.Errorf("failed to render: %w", err)
+		}
+	}
+
+	for _, raw := range rawFiles {
+		if err := b.copyRawFile(raw); err != nil {
+			return fmt.Errorf("failed to copy raw file: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (b *Builder) renderDocument(doc source.Document) error {
+	var err error
+
+	switch document := doc.(type) {
+	case *source.HTMLDocument:
+		err = b.renderHTMLDocument(document)
+	case *source.MarkdownDocument:
+		err = b.renderMarkdownDocument(document)
+	}
+
+	return err
+}
+
+func (b *Builder) renderHTMLDocument(doc *source.HTMLDocument) error {
+	return nil
+}
+
+func (b *Builder) renderMarkdownDocument(doc *source.MarkdownDocument) error {
+	return nil
+}
+
+func (b *Builder) copyRawFile(raw *source.Raw) error {
+	dest, err := raw.CanonicalPath(b.src.ContentDir())
 	if err != nil {
 		return err
 	}
 
-	for _, doc := range docs {
-		p, err := doc.CanonicalPath(b.src.ContentDir())
-		if err != nil {
-			return err
-		}
+	f, err := b.dist.CreateOutputFile(dest)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-		f, err := b.dist.CreateOutputFile(p)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-
-		if err := doc.Render(f); err != nil {
-			return err
-		}
+	if err := raw.Render(f); err != nil {
+		return err
 	}
 
 	return nil
